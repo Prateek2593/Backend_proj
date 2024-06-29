588d8b9ac4bd2d109fddef2ee5813345e7f5e0fe
Prateek2593 <patidarprateek2593@gmail.com>
2024-06-29 14:18:23 +0530

    hooks and jwt tokens

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema(
     {
@@ -45,5 +47,47 @@ const userSchema = new Schema(
         }
     }, {timestamps : true}
 );
+
+//pre is a middleware which will be executed before anything
+//it take an event (say, save) and a call back function
+//dont use arrow function, as it doesnot have 'this' reference
+userSchema.pre("save", async function(next){
+    //is password not modified, then go to next()
+    if(!this.isModified("password")) return next();
+    //encryption of password
+    this.password = bcrypt.hash(this.password, 10);
+    next()
+})
+
+//method to check whether the entered password is correct or not
+//compare method takes two arguments, one the password which user will enter and other is the password which is saved in db
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password) // returns boolean value
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id : this._id,
+        email : this.email,
+        username : this.username,
+        fullname : this.fullname
+    }, 
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id : this._id,
+    }, 
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn : process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
+}
 
 export const User = mongoose.model("User", userSchema)

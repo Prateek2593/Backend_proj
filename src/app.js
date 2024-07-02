fb272aee31083ab1360b310fc734837599406561
Prateek2593 <patidarprateek2593@gmail.com>
2024-07-02 13:46:38 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -4,26 +4,24 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use(cors({
-    origin : process.env.CORS_ORIGIN,
-    credentials : true
-}))
+app.use(cors({origin : process.env.CORS_ORIGIN,credentials : true}))
 
 // accepting json data(while filling a form) with a limit of say, 10kb
-app.use(express.json({
-    limit : "10kb"
-}))
+app.use(express.json({limit : "10kb"}))
 
 // accepting data from url with a limit of say, 10kb
-app.use(express.urlencoded({
-    extended : true,
-    limit : "10kb"
-}))
+app.use(express.urlencoded({extended : true,limit : "10kb"}))
 
 //To serve static files such as images, CSS files, and JavaScript files,
 app.use(express.static("public"))
 
 //to perform CRUD operation on cookies of browser
 app.use(cookieParser())
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
 
 export {app}

956cba2ac90887c7ff839eab312697c9a6240a18
Prateek Patidar <patidarprateek2593@gmail.com>
2024-07-13 22:29:44 +0530

    register user using postman

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,7 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 const registerUser = asyncHandler(async(req,res) =>{
     // get user details from frontend(postman)
     const {fullName, email, username, password} = req.body
-    console.log("email ", email);
+    //console.log("email ", email);
 
     // if (fullName === "") {
     //     throw new ApiError(400, "Fullname is required")
@@ -30,7 +30,7 @@ const registerUser = asyncHandler(async(req,res) =>{
     }
 
     // check if user already exists : email and username
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or : [{username}, {email}]
     })
     if (existedUser) {
@@ -39,14 +39,20 @@ const registerUser = asyncHandler(async(req,res) =>{
 
     // check for images, check for avatar(required)
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
+
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
 
     // upload to cloudinary, check if avatar uploaded or not
     const avatar = await uploadOnCloudinary(avatarLocalPath)
-    const coverImage = await uploadOnCloudinary(coverImageLocalPathLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
     if(!avatar) {
         throw new ApiError(400, "Avatar file is required")
     }
